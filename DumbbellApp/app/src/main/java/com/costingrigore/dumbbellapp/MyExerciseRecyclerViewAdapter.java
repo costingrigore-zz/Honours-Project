7f5da0fcd1565bd7e8a38a120dadc6998e6d5768
Costin Grigore <costingrigore6@gmail.com>
2021-01-14 23:40:53 +0000

    Added Fragment List to Exercises page

---
+++ DumbbellApp/app/src/main/java/com/costingrigore/dumbbellapp/MyExerciseRecyclerViewAdapter.java
@@ -0,0 +1,68 @@
+package com.costingrigore.dumbbellapp;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.costingrigore.dumbbellapp.dummy.DummyContent.DummyItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link DummyItem}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class MyExerciseRecyclerViewAdapter extends RecyclerView.Adapter<MyExerciseRecyclerViewAdapter.ViewHolder> {
+
+    private final ArrayList<Exercise> mValues;
+
+    public MyExerciseRecyclerViewAdapter(ArrayList<Exercise> items) {
+        mValues = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_exercises, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mImageView.setImageResource(mValues.get(position).getIcon());
+        holder.mNameView.setText(mValues.get(position).name);
+        holder.mDifficultyView.setText(mValues.get(position).difficulty);
+        holder.mTypeView.setText(mValues.get(position).type);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final ImageView mImageView;
+        public final TextView mNameView;
+        public final TextView mDifficultyView;
+        public final TextView mTypeView;
+        public Exercise mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mImageView = (ImageView) view.findViewById(R.id.exerciseID);
+            mNameView = (TextView) view.findViewById(R.id.name);
+            mDifficultyView = (TextView) view.findViewById(R.id.difficulty);
+            mTypeView = (TextView) view.findViewById(R.id.type);
+        }
+
+    }
+}
