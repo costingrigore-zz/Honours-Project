2fa6dce2494d658a684241ef167b708e9a52d76a
Costin Grigore <costingrigore6@gmail.com>
2021-01-22 19:17:01 +0000

    Added all the pages needed to request data from the user

--- DumbbellApp/app/src/main/java/com/costingrigore/dumbbellapp/StepTwo.java
+++ DumbbellApp/app/src/main/java/com/costingrigore/dumbbellapp/StepTwo.java
@@ -3,10 +3,18 @@ package com.costingrigore.dumbbellapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.io.IOException;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,14 +23,8 @@ import android.view.ViewGroup;
  */
 public class StepTwo extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    View view;
+    Button nextButton;
 
     public StepTwo() {
         // Required empty public constructor
@@ -40,8 +42,6 @@ public class StepTwo extends Fragment {
     public static StepTwo newInstance(String param1, String param2) {
         StepTwo fragment = new StepTwo();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +49,24 @@ public class StepTwo extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_step_two, container, false);
+        view = inflater.inflate(R.layout.fragment_step_two, container, false);
+        nextButton = (Button) view.findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final FragmentTransaction ft = getFragmentManager().beginTransaction();
+                Fragment fragment = new TrainerFragment();
+                ft.replace(R.id.container, fragment, "Trainer Fragment").commit();
+                BottomNavigationView bottomNavigationView = getActivity().findViewById(R.id.bottomNav);
+                bottomNavigationView.setVisibility(View.VISIBLE);
+            }
+        });
+        return view;
     }
 }
